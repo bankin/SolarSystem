1aa482b1f9dc1089a6acf52c3f9d43c1b1826181
RsknCankov <rosen_cankov@mail.bg>
2014-04-02 11:57:48 +0300

    Novi dobavki

--- SolarSystemProject/SolarSystemProject/SpaceObject.h
+++ SolarSystemProject/SolarSystemProject/SpaceObject.h
@@ -11,9 +11,10 @@ private:
 	char* _dateOfBirth;
 	int _x;
 	int _y;
+	char* _Composition;
 public:
 	//Constructors
-	SpaceObject(int x, int y, int weight = 0, int diameter = 0, int thickness = 0, int temperature = 0, int rotationSpeed = 0, char* dateOfBirth = "");
+	SpaceObject(int x, int y, int weight = 0, int diameter = 0, int thickness = 0, int temperature = 0, int rotationSpeed = 0, char* dateOfBirth = "",char* Composition = "");
 
 	//Setters
 	void set_x(int x) { this->_x = x; }
@@ -24,7 +25,7 @@ public:
 	void set_thickness(int thickness){ this->_thickness = thickness; }
 	void set_diameter(int diameter){ this->_diameter = diameter; }
 	void set_weight(int weight){ this->_weight = weight; }
-
+    void set_Composition(char* Composition){ this ->_Composition = Composition}
 	//Getters
 	int get_x() { return this->_x; }
 	int get_y() { return this->_y; }
@@ -34,5 +35,6 @@ public:
 	int get_thickness(){ return this->_thickness; }
 	int get_diameter(){ return this->_diameter; }
 	int get_weight(){ return this->_weight; }
+	char* get_Composition(){ return this->_Composition;}
 };
 #endif
