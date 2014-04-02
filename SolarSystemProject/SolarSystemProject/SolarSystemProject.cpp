689078ca27bba48907dba817996608bcc43a725a
RsknCankov <rosen_cankov@mail.bg>
2014-04-02 16:28:07 +0300

    Include na vsichki header failove v solarsystemproject.cpp

--- SolarSystemProject/SolarSystemProject/SolarSystemProject.cpp
+++ SolarSystemProject/SolarSystemProject/SolarSystemProject.cpp
@@ -1,7 +1,12 @@
 // SolarSystemProject.cpp : Defines the entry point for the console application.
 //
 
-#include "stdafx.h"
+#include "Planet.h"
+#include "Satellite.h"
+#include "Person.h"
+#include "Star.h"
+#include "SpaceObject.h"
+#include "ChemicalSustav"
 
 
 int _tmain(int argc, _TCHAR* argv[])
